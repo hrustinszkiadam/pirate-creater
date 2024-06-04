fbf3704c7a366c3ef2c28a557baab44c0a656ca6
hrustinszkiadam <hrustinszkiadam@gmail.com>
2024-06-04 08:41:35 +0200

    style: margin increased on large screens

--- src/components/Form/FormLabel.tsx
+++ src/components/Form/FormLabel.tsx
@@ -1,7 +1,9 @@
 import { PropsWithChildren } from 'react';
 
 const FormLabel = ({ children }: PropsWithChildren) => {
-	return <h2 className='mb-1 text-5xl text-orange-950'>{children}</h2>;
+	return (
+		<h2 className='mb-1 text-5xl text-orange-950 lg:mb-2'>{children}</h2>
+	);
 };
 
 export default FormLabel;
